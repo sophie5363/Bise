7d79bb721b5b97e5aaa41ec3adfb17bd1bf0789f
Sophie <sophienanterre@gmail.com>
2021-03-22 21:00:09 +0100

    gestion des caractères interdits dans les adresses mail pour stockage dans firebase

--- Bise/Resources/DatabaseManager.swift
+++ Bise/Resources/DatabaseManager.swift
@@ -23,7 +23,11 @@ extension DatabaseManager {
     public func userExists(with email: String,
                            completion: @escaping ((Bool) -> Void)) {
         
-        database.child(email).observeSingleEvent(of: .value, with: {snapshot in
+        
+        var safeEmail = email.replacingOccurrences(of: ".", with: "-")
+        safeEmail = safeEmail.replacingOccurrences(of: "@", with: "-")
+        
+        database.child(safeEmail).observeSingleEvent(of: .value, with: {snapshot in
             guard  snapshot.value as? String != nil else{
                 completion(false)
                 return
@@ -36,7 +40,7 @@ extension DatabaseManager {
     
     /// Insert new user to database
     public func insertUser(with user: BiseUser){
-        database.child(user.emailAddress).setValue([
+        database.child(user.safeEmail).setValue([
             "prénom": user.firstName,
             "nom" : user.lastName,
         ])
@@ -45,15 +49,17 @@ extension DatabaseManager {
     
 }
 
-
-
-
-
-
-
 struct BiseUser {
     let firstName: String
     let lastName: String
     let emailAddress: String
+    
+    var safeEmail: String {
+        var safeEmail = emailAddress.replacingOccurrences(of: ".", with: "-")
+        safeEmail = safeEmail.replacingOccurrences(of: "@", with: "-")
+        return safeEmail
+    }
+    
     //    let profilePictureUl: String
+    
 }
